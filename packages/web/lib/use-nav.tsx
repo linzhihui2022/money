b3a85e6c9fb8f8c7ba612ce2f83aea43cf22c1b6
lychee <lychee@rb2.nl>
2024-12-02 11:05:59 +0800

    add pending

---
+++ packages/web/lib/use-nav.tsx
@@ -0,0 +1,88 @@
+"use client";
+import {
+  ComponentPropsWithoutRef,
+  createContext,
+  forwardRef,
+  PropsWithChildren,
+  TransitionStartFunction,
+  useCallback,
+  useContext,
+  useTransition,
+  Suspense as ReactSuspense,
+} from "react";
+import { useRouter } from "next/navigation";
+import NextLink from "next/link";
+import { format } from "node:url";
+import { Loader2Icon } from "lucide-react";
+
+interface PendingContextValue {
+  isPending: boolean;
+  startTransition: TransitionStartFunction;
+}
+const PendingContext = createContext<PendingContextValue>(
+  {} as PendingContextValue,
+);
+
+const PendingProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <PendingContext.Provider value={{ isPending, startTransition }}>
+      {children}
+      {isPending && (
+        <div className="fixed bottom-5 right-5 p-2 shadow border rounded-full">
+          <Loader2Icon className="w-4 h-4 animate-spin" />
+        </div>
+      )}
+    </PendingContext.Provider>
+  );
+};
+
+const Suspense = ({
+  children,
+  fallback,
+}: PropsWithChildren<{ fallback: React.ReactNode }>) => {
+  const { isPending } = useContext(PendingContext);
+  return (
+    <ReactSuspense fallback={fallback}>
+      {isPending ? fallback : children}
+    </ReactSuspense>
+  );
+};
+
+const useNav = () => {
+  const router = useRouter();
+  const { startTransition, isPending } = useContext(PendingContext);
+  const navigate = useCallback(
+    (path: ComponentPropsWithoutRef<typeof NextLink>["href"]) => {
+      if (path) {
+        startTransition(async () => {
+          router.push(format(path));
+        });
+      }
+    },
+    [router, startTransition],
+  );
+  return { navigate, isPending };
+};
+
+const Link = forwardRef<
+  HTMLAnchorElement,
+  ComponentPropsWithoutRef<typeof NextLink>
+>(({ onClick, ...props }, ref) => {
+  const { navigate } = useNav();
+  return (
+    <NextLink
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        navigate(props.href);
+        onClick?.(e);
+      }}
+      {...props}
+    />
+  );
+});
+
+Link.displayName = NextLink.displayName;
+
+export { Link, PendingProvider, useNav, Suspense };
