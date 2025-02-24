ce155cf8986e28611c8f65987857e01245f36ad9
lychee <linzhihui2022@outlook.com>
2025-02-24 22:17:37 +0800

    feat: remove i18n path

--- packages/web/app/[locale]/(web)/cookbook/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function Page() {
-  return <div className="relative flex w-full flex-col min-h-dvh"></div>;
-}
--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,9 +1,51 @@
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { PendingProvider } from "@/lib/use-nav";
+import { getLocale, getMessages, getTranslations } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+import { ThemeProvider } from "@/components/theme-provider";
+import Head from "next/head";
+import { getUserLocale } from "../i18n/cookies";
 
+export async function generateMetadata() {
+  const locale = await getLocale();
+  const t = await getTranslations({ locale, namespace: "layout" });
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 export default async function RootLayout({
   children,
 }: {
   children: ReactNode;
 }) {
-  return <>{children}</>;
+  const locale = await getUserLocale();
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextIntlClientProvider messages={messages}>
+            <PendingProvider>
+              {children}
+              <Toaster />
+            </PendingProvider>
+          </NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
