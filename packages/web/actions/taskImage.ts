303fb9bea96a9a4bd0f22c5ac3f15114de6a13e4
lychee <linzhihui2022@outlook.com>
2025-03-06 20:19:54 +0800

    fix: tags

--- packages/web/actions/taskImage.ts
+++ packages/web/actions/taskImage.ts
@@ -12,10 +12,10 @@ export const createTaskImage = async (
 ) => {
   const { publicUrl } = await uploadFile(file, key, "task");
   await prisma.taskImage.create({ data: { taskId, url: publicUrl, key } });
-  revalidateTag("task");
+  revalidateTag("tasks");
 };
 
 export const deleteTaskImage = async (imageKey: string) => {
   await prisma.taskImage.delete({ where: { key: imageKey } });
-  revalidateTag("task");
+  revalidateTag("tasks");
 };
